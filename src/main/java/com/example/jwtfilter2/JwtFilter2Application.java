30945e2d92e09699e2b817af77b7711bffc77c09
이희수 <leehsdd33@gmail.com>
2025-03-08 22:56:04 +0900

    first commit

---
+++ src/main/java/com/example/jwtfilter2/JwtFilter2Application.java
@@ -0,0 +1,13 @@
+package com.example.jwtfilter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtFilter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtFilter2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtfilter2/JwtFilter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtfilter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtFilter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
